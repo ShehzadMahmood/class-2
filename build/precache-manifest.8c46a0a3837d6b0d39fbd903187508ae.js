5289e4d9db3a32dd48844ea79f2052ba69224350
Shehzad Mahmood <65547678+ShehzadMahmood@users.noreply.github.com>
2020-08-12 15:15:29 +0500

    update

---
+++ build/precache-manifest.8c46a0a3837d6b0d39fbd903187508ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25922ebdd63b5e0cc2f613f0ab6fcf27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38ea53753f9a5a309f0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8105544f4634d82b601b",
+    "url": "/static/js/2.9850564f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9850564f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38ea53753f9a5a309f0",
+    "url": "/static/js/main.c708f821.chunk.js"
+  },
+  {
+    "revision": "3ff5ed55dc611b015d27",
+    "url": "/static/js/runtime-main.e2123a7d.js"
+  }
+]);
